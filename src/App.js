a7e720797e259c392003350958e87564e8f825f0
Alexandre Vessoni <av.vessoni@gmail.com>
2019-10-16 14:23:35 -0300

    add analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
+
+ReactGA.initialize('UA-105168101-1');
+ReactGA.pageview('/');
 
 function App() {
   return (
